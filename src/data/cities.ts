d929120fc8aae7f87354502931bf4245ed5c8433
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-09-10 18:17:27 +0200

    Working on search logic

--- src/data/cities.ts
+++ src/data/cities.ts
@@ -1,5 +1,6 @@
-// data/cities.ts
+// Imports data from cities.json into cityData
 import cityData from './cities.json';
-import { City } from '../types/types';
+import type { City } from '../types/types';
 
+// Exports the city data as an array named cities of City objects
 export const cities: City[] = cityData;

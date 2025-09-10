64b34d63ee22ed3bab46c8f5d01b642671189e04
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-09-10 13:55:00 +0200

    Almost complete hamburgermenu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,28 @@
 import type React from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import './Navbar.css';
+import "./Navbar.css";
 
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<Boolean>(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <nav className="navbar">
-        <section className="logo">World Clock</section>
-        <section className="nav-links">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/favorites">Favorites</NavLink>
-        </section>
-        <section className="hamburger">
-            <span></span>
-            <span></span>
-            <span></span>
-        </section>
-
+      <section className="logo">World Clock</section>
+      <section className={`nav-links ${isOpen ? "open" : ""}`}>
+        <NavLink to="/" onClick={closeMenu}>Home</NavLink>
+        <NavLink to="/favorites" onClick={closeMenu}>Favorites</NavLink>
+      </section>
+      <section className="hamburger" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </section>
+      {isOpen && <section className="overlay" onClick={closeMenu}></section>}
     </nav>
-  )
+  );
 };
 
 export default Navbar;

5b4a8a37b09f8abd9bb7cdd12da249d4a5c8f42b
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-09-10 14:14:50 +0200

    Navbar almost complete, added useState, functions for menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,22 +4,35 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar: React.FC = () => {
-  const [isOpen, setIsOpen] = useState<Boolean>(false);
+  // State to set the to open and closed
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  // Function to toggle open and close on menu
   const toggleMenu = () => setIsOpen(!isOpen);
+  // Function to close menu when clicking a link
   const closeMenu = () => setIsOpen(false);
 
   return (
     <nav className="navbar">
       <section className="logo">World Clock</section>
+      {/* If isOpen is true, add "open" to className */}
       <section className={`nav-links ${isOpen ? "open" : ""}`}>
-        <NavLink to="/" onClick={closeMenu}>Home</NavLink>
-        <NavLink to="/favorites" onClick={closeMenu}>Favorites</NavLink>
+        <NavLink to="/" onClick={closeMenu}>
+          Home
+        </NavLink>
+        <NavLink to="/favorites" onClick={closeMenu}>
+          Favorites
+        </NavLink>
       </section>
-      <section className="hamburger" onClick={toggleMenu}>
+      <section
+        // If isOpen is true, add "open" to className
+        className={`hamburger ${isOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
         <span></span>
         <span></span>
         <span></span>
       </section>
+      {/* If isOpen is true, show the overlay and close the menu when clicking outside */}
       {isOpen && <section className="overlay" onClick={closeMenu}></section>}
     </nav>
   );
